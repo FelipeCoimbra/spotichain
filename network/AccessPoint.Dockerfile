55c10439bc1b59ece0729a8d753798531b7154c4
Felipe Coimbra <felipecoimbra97@gmail.com>
2020-12-13 23:17:08 -0300

    blockchain working with 1 container

--- network/AccessPoint.Dockerfile
+++ network/AccessPoint.Dockerfile
@@ -4,8 +4,10 @@ WORKDIR ether/
 # Genesis block setup
 COPY genesis.json ./genesis.json
 RUN geth --nousb --datadir "./AccessNode" init "./genesis.json"
-RUN geth --nousb --datadir "./Miner0" init "./genesis.json"
 RUN geth --nousb --datadir "./BootNode" init "./genesis.json"
+
+# Access credentials
+COPY accesspwd AccessNode/accesspwd
 
 # Bootnode key
 COPY bootnode.nodekey BootNode/bootnode.nodekey
--- network/startAccess.sh
+++ network/startAccess.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 ACCESSNODE_DIR=/ether/AccessNode
-MINER_DIR=/ether/Miner0
 BOOTNODE_DIR=/ether/BootNode
 
 ##
 ## Start Boot node
 ##
-bootnode --addr $HOSTNAME --nodekey=$BOOTNODE_NODEKEY_FILE > $BOOTNODE_DIR/enode &
+bootnode --addr $HOSTNAME:30301 --nodekey=$BOOTNODE_NODEKEY_FILE > $BOOTNODE_DIR/enode &
 
 ##
 ## Get bootnode enode
@@ -17,28 +16,34 @@ while [[ x$BOOT_ENODE == x ]] ; do
 done
 
 ##
-## Start Access Node
+## Setup Access Node account
+##
+geth --nousb \
+--nodiscover \
+--datadir $ACCESSNODE_DIR \
+--networkid $NETWORKID \
+account new --password $ACCESSNODE_DIR/accesspwd \
+2>>$ACCESSNODE_DIR/geth.log
+
+echo "oi"
+
+##
+## Start Access Node + Mining thread
 ##
 geth --nousb \
 --datadir $ACCESSNODE_DIR \
---port 30301 \
---rpc \
---rpcport "8545" \
---rpccorsdomain "*" \
+--port 30302 \
+--http \
+--http.addr "0.0.0.0" \
+--http.port "8545" \
+--http.api eth,web3,personal,net \
+--http.corsdomain "*" \
 --networkid $NETWORKID \
 --allow-insecure-unlock \
 --bootnodes $BOOT_ENODE \
-2>>$ACCESSNODE_DIR/geth.log &
-
-##
-## Start Miner
-##
-geth --datadir=datadir \
---networkid $NETWORKID \
---bootnodes $BOOT_ENODE \
 --mine \
---minerthreads=1 \
---etherbase=0 \
-2>>$BOOTNODE_DIR/geth.log &
+--miner.threads=1 \
+--miner.etherbase=0 &
 
+echo "Spotichain ready for access!"
 while [[ true ]] ; do sleep 1; done
